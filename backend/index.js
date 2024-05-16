0f0c46b36c4d308517eeadf9ffc2667a5b8b7c63
Nishit Grover <grovernt@mail.uc.edu>
2024-05-16 01:05:53 -0400

    chore: Add routes for retrieving, creating, updating, and deleting books

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,7 @@ app.post("/books", async (req,res)=>{
 app.get("/books", async (req,res)=>{
    try{
       const books = await Book.find({});
+
       return res.status(200).json({
          count : books.length,
          data: books
@@ -51,6 +52,61 @@ app.get("/books", async (req,res)=>{
    }
 });
 
+// Route to get books BY ID
+app.get("/books/:id", async (req,res)=>{
+   try{
+      const { id } = req.params;
+      const books = await Book.findById(id);
+      
+      return res.status(200).json(books);
+   }
+   catch(err){
+      console.log(err.message)
+      return res.status(500).send(err.message);
+   }
+});
+
+// Route to update a book
+app.put("/books/:id", async (req,res)=>{
+   try{
+      if (
+         !req.body.title ||
+         !req.body.author ||
+         !req.body.publishYear
+      ) {
+         return res.status(400).send("All input is required");
+      }
+      
+      const { id } = req.params;
+      const result = await Book.findByIdAndUpdate(id, req.body)
+
+      if(!result){
+         return res.status(404).send("Book not found")
+      }
+      return res.status(200).send("Updated Succesfully")
+
+   } catch(err){
+      console.log(err.message)
+      return res.status(500).send(err.message);
+   }
+});
+
+// Route to delete a book
+app.delete("/books/:id", async (req,res)=>{
+   try{
+      const { id } = req.params;
+      const result = await Book.findByIdAndDelete(id)
+
+      if(!result){
+         return res.status(404).send("Book not found")
+      }
+      return res.status(200).send("Deleted Succesfully")
+   }
+   catch(err){
+      console.log(err.message)
+      return res.status(500).send(err.message);
+   }
+});
 mongoose
    .connect(mongoDBURL)
     .then(()=>{
