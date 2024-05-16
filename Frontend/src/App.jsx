ed068abcbd6498f0c12618396ab96dc36c713cad
Nishit Grover <grovernt@mail.uc.edu>
2024-05-16 13:25:42 -0400

    chore: Update npm dependencies and add react-router-dom package

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {CreateBook} from './pages/CreateBooks'
+import {EditBooks} from './pages/UpdateBooks'
+import {DeleteBooks} from './pages/DeleteBooks'
+import {ShowBooks} from './pages/ListBooks'
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/books/create" element = {<CreateBook/>} />
+      <Route path="/books/details/:id " element = {<ShowBooks/>} />
+      <Route path="/books/edit/:id " element = {<EditBooks/>} />
+      <Route path="/books/delete/:id" element = {<DeleteBooks/>} />
+    </Routes>
+  )
 };
 
 export default App;
