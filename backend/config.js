689afdcccbc1b70ba1bde8f888b6291c494d54d4
Nishit Grover <grovernt@mail.uc.edu>
2024-05-15 23:53:14 -0400

    chore: Update npm dependencies

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+"mongodb+srv://grover:grover@bookstore.uizpppm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore"
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
+app.get("/", (req,res)=> {
+   return res.status(234).send("Welcome")
+});
+
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
+   });
+
+
+
+mongooose
+   .connect(mongoDBURL)
+   .then(()=>{
+
+   })
+   .catch((err)=>{
+      console.log(err);
    });
